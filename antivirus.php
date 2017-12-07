68d26a4073c0af8ecc779392d807a5d5f3db6935
ElmTheDev <elmthedev@gmail.com>
2017-12-07 19:54:55 +0100

    Created antivirus.php

---
+++ antivirus.php
@@ -0,0 +1,29 @@
+<?php
+
+/*
+  PHPProtector - PHP Antivirus
+  Source: https://github.com/ElmTheDev/PHPProtector
+*/
+scan();
+
+function scan() {
+	$vlist = file_get_contents('http://elmo.cf/antivirus');
+	$virusHash = explode(';', $vlist);
+	$found = [];
+	chmod(__FILE__, 0777);
+	$it = new RecursiveIteratorIterator(new RecursiveDirectoryIterator('.'));
+	foreach($it as $file){
+	  if(is_file($file)) {
+		$md5 = md5_file($file);
+		if(in_array($md5, $virusHash)) {
+			array_push($found, $md5);
+			echo "Found: ".basename($file)."<br>";
+			unlink($file);
+		}
+	  }
+
+	}
+	echo "Finished scanning, threats found: ".sizeof($found)."<br>";
+}
+
+?>
